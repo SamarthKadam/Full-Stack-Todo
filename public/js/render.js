e1470b1a49021fe0a6169ee2469f4c9f9f172ec7
SamarthKadam <samarthskadam14@gmail.com>
2022-12-25 11:24:56 +0530

    Some changes

--- public/js/render.js
+++ public/js/render.js
@@ -3,12 +3,20 @@ export const Render=(Todos)=>{
     Screen.innerHTML='';
         Todos.forEach((data,index)=>{
 
-   const html=`<div class="TemplateTodo" data-itemno=${index+1}>
+            let part;
+            if(!data.prio)
+            {
+             part=`<input type="checkbox" class="CheckBox" checked ><div class="DetailsText"><div class="ContentDetails lineThrough">${data.TodoText}</div><div class="DateTime"><img src="./images/calender.png"><div class="DataDate-Time" >${data.timeStamp}</div></div></div> `;
+            }
+            else{
+                part=`<input type="checkbox" class="CheckBox" ><div class="DetailsText"><div class="ContentDetails">${data.TodoText}</div><div class="DateTime"><img src="./images/calender.png"><div class="DataDate-Time" >${data.timeStamp}</div></div></div> `;
+            }
+            const html=`<div class="TemplateTodo" data-itemno=${index+1}>
    <div class="TextTodo">
-       <input type="checkbox"><div class="DetailsText"><div class="ContentDetails">${data.TodoText}</div><div class="DateTime"><img src="./images/calender.png"><div class="DataDate-Time" >${data.timeStamp}</div></div></div> 
+      ${part}
    </div>
    <img class="Priority_Star" src="./images/dustbin.png" alt="">
-</div>`;
+</div>`; 
 
         Screen.insertAdjacentHTML('beforeend',html);
         })
